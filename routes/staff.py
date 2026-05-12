3a7256cf99eebf80bc0bb6f7f7ccb8b5b9e56b24
20240057 <20240057@stud.fci-cu.edu.eg>
2026-05-12 15:24:40 +0300

    Implement add new staff feature
    Added functionality to add new staff members to the database.

--- routes/staff.py
+++ routes/staff.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 import pyodbc
 
 staff_bp = Blueprint('staff', __name__)
+
 def get_db_connection():
     conn_str = (
         "Driver={ODBC Driver 17 for SQL Server};"
@@ -11,14 +12,18 @@ def get_db_connection():
         "TrustServerCertificate=yes;"
     )
     return pyodbc.connect(conn_str)
+
 @staff_bp.route('/staff')
 def manage_staff():
     conn = get_db_connection()
     cursor = conn.cursor()
+    
     cursor.execute("SELECT StaffID, FirstName, LastName FROM dbo.Staff")
     staff_list = cursor.fetchall()
+    
     cursor.execute("SELECT GatheringID, GatheringName FROM dbo.Gathering")
     events = cursor.fetchall()
+
     cursor.execute("""
         SELECT sa.AssignmentID, s.FirstName + ' ' + s.LastName, g.GatheringName, sa.AssignmentRole
         FROM dbo.StaffAssignment sa
@@ -39,16 +44,33 @@ def manage_staff():
         ORDER BY GatheringsManaged DESC
     """)
     top_coord = cursor.fetchone()
+
     conn.close()
     return render_template('staff_list.html', 
                            staff=staff_list, 
                            gatherings=events, 
                            schedule=schedule, 
                            top_coord=top_coord)
+
+@staff_bp.route('/add-staff', methods=['POST'])
+def add_new_staff():
+    f_name = request.form.get('first_name')
+    l_name = request.form.get('last_name')
+    email = request.form.get('email')
+    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO dbo.Staff (FirstName, LastName, Email) VALUES (?, ?, ?)", 
+                   (f_name, l_name, email))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('staff.manage_staff'))
+
 @staff_bp.route('/assign-staff', methods=['POST'])
 def assign_staff():
     s_id = request.form.get('staff_id')
     g_id = request.form.get('gathering_id')
+    
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("""
@@ -57,11 +79,11 @@ def assign_staff():
     conn.commit()
     conn.close()
     return redirect(url_for('staff.manage_staff'))
+
 @staff_bp.route('/remove-assignment/<int:id>', methods=['POST'])
 def remove_assignment(id):
     conn = get_db_connection()
     cursor = conn.cursor()
-    # Deleting from the official table using the correct ID column
     cursor.execute("DELETE FROM dbo.StaffAssignment WHERE AssignmentID = ?", (id,))
     conn.commit()
     conn.close()
